81caa88ef252fce2f38ec26c3119232551eebb91
郭黎辉 <guolihui@guolihuideMacBook-Pro.local>
2018-01-13 00:46:18 +0800

    add

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -78,6 +78,7 @@ class TopicsController extends Controller
         if ($file = $request->upload_file) {
             // 保存图片到本地
             $result = $uploader->save($request->upload_file, 'topics', \Auth::id(), 1024);
+
             // 图片保存成功的话
             if ($result) {
                 $data['file_path'] = $result['path'];
